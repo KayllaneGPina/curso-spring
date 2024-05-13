572e7efabd48a4f11730e4c8ad62a1c00e752a8b
KayllaneGPina <kayllanegfpina@gmail.com>
2024-05-13 19:53:45 -0300

    :sparkles:feat: Modelando temporadas

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,9 +1,17 @@
 package br.com.alura.screenmatch;
 
+import br.com.alura.screenmatch.model.DadosEpisodios;
+import br.com.alura.screenmatch.model.DadosSerie;
+import br.com.alura.screenmatch.model.DadosTemporada;
 import br.com.alura.screenmatch.service.ConsumoAPI;
+import br.com.alura.screenmatch.service.ConverterDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
@@ -16,8 +24,26 @@ public class ScreenmatchApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 
 		var consumoAPI = new ConsumoAPI();
-		var json = consumoAPI.obterDados("http://www.omdbapi.com/?t=narcos&Season=1&apikey=9231eb0f");
-		System.out.println(json);
+		var json = consumoAPI.obterDados("http://www.omdbapi.com/?t=narcos&apikey=9231eb0f");
+//		System.out.println(json);
 
+		ConverterDados conversor = new ConverterDados();
+
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+//		System.out.println(dados);
+
+		DadosEpisodios dadosEpisodios = conversor.obterDados(json, DadosEpisodios.class);
+//		System.out.println(dadosEpisodios);
+
+		List<DadosTemporada> temporadas = new ArrayList<>();
+
+		for (int i = 1; i <= dados.totalTemporadas(); i++) {
+			json = consumoAPI.obterDados("http://www.omdbapi.com/?t=narcos&season=" + i + "&apikey=9231eb0f");
+
+			DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+			temporadas.add(dadosTemporada);
+		}
+
+		temporadas.forEach(System.out::println);
 	}
 }
